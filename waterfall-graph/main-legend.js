b23eb663b859d24d6250b83e63058471c6f3026d
Stanislav Sysoev <d4rkr00t@gmail.com>
2014-10-13 08:36:45 +0400

    main legend positioning

--- waterfall-graph/main-legend.js
+++ waterfall-graph/main-legend.js
@@ -2,8 +2,14 @@
     var mainLegend = document.querySelector('.w-graph__main-legend'),
         width = mainLegend.clientWidth;
 
-    window.onscroll = function() {
-        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    var onScroll = function() {
+        var winWidth = window.innerWidth,
+            scrollPos = window.pageXOffset,
+            pos = (scrollPos + winWidth / 2 ) - width / 2;
 
-    }
+        mainLegend.style.left = pos + 'px';
+    };
+
+    window.onscroll = onScroll;
+    onScroll();
 })();
