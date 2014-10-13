3947b5a02d89b04416d0e5431d341a646df63afa
Stanislav Sysoev <d4rkr00t@gmail.com>
2014-10-13 13:41:54 +0400

    positioning timings legend

--- waterfall-graph-dev/js/legend.js
+++ waterfall-graph-dev/js/legend.js
@@ -16,22 +16,43 @@ define(function () {
         onScroll();
     };
 
-    var _timingHoverTriggers;
-    var timingsLegend = function() {
-        console.log(_timingHoverTriggers);
+    var _timingHoverTriggers,
+        _timingsLegend;
+    var _detachEvents = function(list, listener) {
+        [].forEach.call(list, function (item) {
+            item.removeEventListener('mouseenter', listener);
+        });
     };
+    var _attachEvents = function(list, listener) {
+        [].forEach.call(list, function (item) {
+            item.addEventListener('mouseenter', listener);
+        });
+    };
+    var _hoverListener = function(e) {
+        setTimeout(function () {
+            var x = e.target.getBBox().x,
+                y = e.target.getBBox().y + 100,
+                legendWidth = _timingsLegend.clientWidth,
+                legendHeight = _timingsLegend.clientHeight,
+                entityWidth = e.target.childNodes[0].getBBox().width,
+                entityHeight = e.target.childNodes[0].getBBox().height,
+                entityCenter = x + ( entityWidth / 2),
+                windowRightBorder = window.innerWidth + window.pageXOffset,
+                yPos = ( ( y + entityHeight / 2 ) - 80 - legendHeight );
 
-    var timingsLegendGetNewElems = function() {
-        if (_timingHoverTriggers) {
-            _timingHoverTriggers = document.querySelectorAll('.w-graph__entry-hover-trigger');
-        } else {
-            _timingHoverTriggers = document.querySelectorAll('.w-graph__entry-hover-trigger');
-        }
-        timingsLegend();
+            _timingsLegend.style.left = (entityCenter - legendWidth / 2) + 'px';
+            _timingsLegend.style.top = ( y < yPos ? y - 30 - legendHeight : yPos ) + 'px';
+        }, 0);
+    };
+    var timingsLegend = function() {
+        _timingHoverTriggers && _detachEvents(_timingHoverTriggers, _hoverListener);
+        !_timingsLegend && (_timingsLegend = document.querySelector('.w-graph__timings-legend'));
+        _timingHoverTriggers = document.querySelectorAll('.w-graph__entry-hover-trigger');
+        _attachEvents(_timingHoverTriggers, _hoverListener);
     };
 
     return {
         mainLegend: mainLegend,
-        timingsLegend: timingsLegendGetNewElems
+        timingsLegend: timingsLegend
     };
 });
