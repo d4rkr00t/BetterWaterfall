61a7603f8f4db94fa53bb9b7e9c36b89ba3fe9da
Stanislav Sysoev <d4rkr00t@gmail.com>
2014-11-03 15:35:55 +0300

    fix in build system

--- gulpfile.js
+++ gulpfile.js
@@ -14,29 +14,29 @@ var gulp = require('gulp'),
     reload = browserSync.reload,
 
     paths = {
-        mainStyle: 'waterfall-graph-dev/css/style.scss',
-        styles: ['waterfall-graph-dev/css/*.scss'],
-        js: ['waterfall-graph-dev/js/*.js']
+      mainStyle: 'waterfall-graph-dev/css/style.scss',
+      styles: ['waterfall-graph-dev/css/*.scss'],
+      js: ['waterfall-graph-dev/js/*.js']
     },
 
     errorHandler = function(err) {
-        console.error(err);
-        this.end();
+      console.error(err);
+      this.end();
     };
 
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: './waterfall-graph-dev/'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: './waterfall-graph-dev/'
+    }
+  });
 });
 
 gulp.task('css', function () {
     return gulp.src(paths.mainStyle)
             .pipe(sass({
-                errLogToConsole: true,
-                sourceComments: 'map'
+              errLogToConsole: true,
+              sourceComments: 'map'
             }))
             .on('error', errorHandler)
             .pipe(gulp.dest('waterfall-graph-dev/css'))
@@ -44,15 +44,15 @@ gulp.task('css', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.styles, ['css']);
+  gulp.watch(paths.styles, ['css']);
 
-    gulp.watch(paths.js, function (file) {
-        gulp.src(file.path).pipe(reload({ stream: true }));
-    });
+  gulp.watch(paths.js, function (file) {
+    gulp.src(file.path).pipe(reload({ stream: true }));
+  });
 
-    gulp.watch('./waterfall-graph-dev/*.html', function (file) {
-        gulp.src(file.path).pipe(reload({ stream: true }));
-    });
+  gulp.watch('./waterfall-graph-dev/*.html', function (file) {
+    gulp.src(file.path).pipe(reload({ stream: true }));
+  });
 });
 
 gulp.task('default', ['css', 'browser-sync', 'watch']);
@@ -64,9 +64,9 @@ gulp.task('build-js', function () {
     .pipe(amdOptimize('main', {
       baseUrl: './',
       paths: {
-          'vendor/d3/d3.min': 'waterfall-graph-dev/vendor/d3/d3.min',
-          'app/utils': 'waterfall-graph-dev/js/utils',
-          'app/legend': 'waterfall-graph-dev/js/legend'
+        'vendor/d3/d3.min': 'waterfall-graph-dev/vendor/d3/d3.min',
+        'app/utils': 'waterfall-graph-dev/js/utils',
+        'app/legend': 'waterfall-graph-dev/js/legend'
       }
     }))
     .pipe(concat('app.js'))
@@ -77,8 +77,11 @@ gulp.task('build-js', function () {
 
 gulp.task('build-css', function() {
   gulp.src('waterfall-graph-dev/css/style.css')
+    .pipe(sass())
+    .on('error', errorHandler)
+    .pipe(gulp.dest('waterfall-graph-dev/css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('chrome-ext/'));
 });
 
-gulp.task('build', ['css', 'build-css', 'build-js']);
+gulp.task('build', ['build-css', 'build-js']);
